1827aca6cb108e0dad24dd39069238c1bda819e7
AbdullahAlNaim <aanaim96@gmail.com>
2024-06-07 16:32:48 -0400

    add user sign up page form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,12 @@ class User < ApplicationRecord
   validates :username, 
     uniqueness: { case_sensitive: false }, 
     presence: true, 
-    uniqueness: true, 
     length: { minimum: 3, maximum: 25 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, 
     uniqueness: { case_sensitive: false }, 
-    uniqueness: true, 
     presence: true, 
     length: { maximum: 105 },
     format: { with: VALID_EMAIL_REGEX }
