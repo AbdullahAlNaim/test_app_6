4b52c454c8c370273eb7bc990838f5b704a0f878
AbdullahAlNaim <aanaim96@gmail.com>
2024-06-15 14:38:01 -0400

    added some authentication and restrictions per user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,12 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha blog, #{@user.username} successfully signed up"
       redirect_to articles_path
     else
@@ -27,8 +28,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = "User account successfully updated"
       redirect_to @user
@@ -39,6 +38,12 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
